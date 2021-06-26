abdd9312b2deccde9ae3eed2a4047033ca49f848
Gregory Rudolph <rudi@nightmare.haus>
2021-06-26 10:03:57 -0400

    Added exception handling and warning toasts

--- app/src/main/java/dev/synople/amazonlookup/MainActivity.kt
+++ app/src/main/java/dev/synople/amazonlookup/MainActivity.kt
@@ -3,14 +3,12 @@ package dev.synople.amazonlookup
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
-import com.google.zxing.integration.android.IntentIntegrator
 import android.widget.Toast
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.google.zxing.integration.android.IntentIntegrator
 import java.io.BufferedReader
 import java.io.InputStreamReader
-import java.lang.StringBuilder
 import java.net.URL
 import kotlin.concurrent.thread
 
@@ -33,6 +31,10 @@ class MainActivity : Activity() {
 
                 thread {
                     val asin = getAsin(upc)
+                    if ("" == asin) {
+                        finishAffinity()
+                        return@thread
+                    }
                     Log.v("MainActivity", "ASIN: $asin")
                     val detailPage = getDetailPage(asin)
                     runOnUiThread {
@@ -42,6 +44,8 @@ class MainActivity : Activity() {
                             Toast.makeText(this, "Not Amazon's Choice!", Toast.LENGTH_SHORT).show()
                         }
                     }
+                    finishAffinity()
+
                 }
             }
         } else {
@@ -50,44 +54,64 @@ class MainActivity : Activity() {
     }
 
     private fun getDetailPage(asin: String): String {
+        if ("" == asin) {
+            return ""
+        }
         val url = URL("https://www.amazon.com/dp/$asin")
         val urlConnection = url.openConnection()
-        val bufferedReader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
-        var inputLine = bufferedReader.readLine()
-        val responseBuilder = StringBuilder()
+        try {
+            val bufferedReader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
+            var inputLine = bufferedReader.readLine()
+            val responseBuilder = StringBuilder()
 
-        while (inputLine != null) {
-            responseBuilder.append(inputLine)
-            inputLine = bufferedReader.readLine()
+            while (inputLine != null) {
+                responseBuilder.append(inputLine)
+                inputLine = bufferedReader.readLine()
+            }
+
+            bufferedReader.close()
+
+            return responseBuilder.toString()
+        } catch (e: Exception) {
+            Log.v("MainActiity.getAsin", "Exception in getting website", e.cause)
+            runOnUiThread {
+                Toast.makeText(this, "Unable to get Detail Page", Toast.LENGTH_LONG).show()
+            }
+
         }
-
-        bufferedReader.close()
-
-        return responseBuilder.toString()
+        return ""
     }
 
     private fun getAsin(upc: String): String {
         val url = URL("https://www.amazon.com/s?k=$upc")
         val urlConnection = url.openConnection()
-        val bufferedReader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
-        var inputLine = bufferedReader.readLine()
-        val responseBuilder = StringBuilder()
+        try {
+            val bufferedReader = BufferedReader(InputStreamReader(urlConnection.getInputStream()))
+            var inputLine = bufferedReader.readLine()
+            val responseBuilder = StringBuilder()
 
-        while (inputLine != null) {
-            responseBuilder.append(inputLine)
-            inputLine = bufferedReader.readLine()
-        }
+            while (inputLine != null) {
+                responseBuilder.append(inputLine)
+                inputLine = bufferedReader.readLine()
+            }
 
-        bufferedReader.close()
+            bufferedReader.close()
 
-        val website = responseBuilder.toString()
-        // (?<=asin=").{10}
+            val website = responseBuilder.toString()
+            // (?<=asin=").{10}
 
-        for (i in 0..website.length - 20) {
-            if (website[i] == 'a' && website[i + 1] == 's' && website[i + 2] == 'i' && website[i + 3] == 'n' && website[i + 4] == '=' && website[i + 5] == '"') {
-                return website.substring(i + 6, i + 16)
+            for (i in 0..website.length - 20) {
+                if (website[i] == 'a' && website[i + 1] == 's' && website[i + 2] == 'i' && website[i + 3] == 'n' && website[i + 4] == '=' && website[i + 5] == '"') {
+                    return website.substring(i + 6, i + 16)
+                }
+            }
+        } catch (e: Exception) {
+            Log.v("MainActiity.getAsin", "Exception in getting website: " + e.localizedMessage, e.cause)
+            runOnUiThread {
+                Toast.makeText(this, "Unable to get ASIN, check internet connection.", Toast.LENGTH_LONG).show()
             }
         }
         return ""
     }
+
 }
